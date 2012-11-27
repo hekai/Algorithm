6b06bacd45a71cc79960b926d3b2776cd1a927bb
hacklu <hacklu.newborn@gmail.com>
2012-11-27 09:24:23 +0800

    directory structure

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'test');
+
+/** MySQL database username */
+define('DB_USER', 'test');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'test');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+?>
+
