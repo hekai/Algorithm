afbd991d22345f9ee408ad2bd09a5e0a058e831f
hekai <hwk256@gmail.com>
2012-12-01 14:11:04 +0800

    add login with sina weibo.

--- logout.php
+++ logout.php
@@ -1,8 +1,11 @@
 <?php
+	session_start();
 	unset($_SESSION['login']);
+	unset($_SESSION['uid']);
 	unset($_SESSION['userID']);
 	unset($_SESSION['name']);
 	unset($_SESSION['nickname']);
+	unset($_SESSION['team']);
 	unset($_SESSION['photoPath']);
 	
 	header('Location: login.php');
